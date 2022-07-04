bd8d56ec519ce6c75c055424cbe93b7629d4250b
Prashant-SL <pacchidon07@gmail.com>
2022-07-04 19:34:10 +0530

    routing

--- httpserver/index.js
+++ httpserver/index.js
@@ -1,7 +1,18 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-    res.write("<h1>Listening on the other side</h1>");
-    res.end;
+    if (req.url == "/") {
+        res.write("<h1>Listening on the other side</h1>");
+    }
+    else if (req.url == "/contact") {
+        res.write("<h1>Listening on the contact side</h1>");
+    }
+    else if (req.url == "/about") {
+        res.write("<h1>Listening on the about side</h1>");
+    }
+    else {
+        res.writeHead(404, { "Content-type": "text/html" })
+        res.end("Error 404. Page does not exist.");
+    }
 });
 
 server.listen(8080, "127.0.0.1", () => {
